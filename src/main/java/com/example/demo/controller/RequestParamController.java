ba4e93a1afb0072d30906b7a72312daf532f3a61
gon <gondo-t@evoltech.co.jp>
2026-06-10 11:06:22 +0900

    入力値が出力画面に表示される

--- src/main/java/com/example/demo/controller/RequestParamController.java
+++ src/main/java/com/example/demo/controller/RequestParamController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RequestParamController {
@@ -11,5 +14,25 @@ public class RequestParamController {
 	public String showView() {
 		// 表示する「ビュー名」
 		return "input";
+	}
+	
+	// GET かつ [url : /result]
+	@GetMapping("result")
+	public String showResultGet(
+		@RequestParam String val, Model model) {
+		// モデルに送られてきた値を設定
+		model.addAttribute("value", val);
+		// 表示する「ビュー名」
+		return "output";
+	}
+	
+	// POST かつ [url : /result]
+	@PostMapping("result")
+	public String showResultPost(
+		@RequestParam String val, Model model) {
+		// モデルに送られてきた値を設定
+		model.addAttribute("value", val);
+		// 表示する「ビュー名」
+		return "output";
 	}
 }
